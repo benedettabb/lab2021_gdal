0f1e5cd403d3ab9499f4a492fdd4a44137fa2a21
benedettabb <80822451+benedettabb@users.noreply.github.com>
2021-06-03 16:30:52 +0200

    Update csv.py

--- csv.py
+++ csv.py
@@ -2,8 +2,32 @@ import csv
 #carico il dataset
 dataset = open("C:/Users/UTENTE/Desktop/UNIBO/GEOGRAFIA/II ANNO/LAB/python/montecristo_point.csv")
 #apro la tabella
-table = csv.reader (dataset, delimiter = ',')
+table = csv.reader (dataset, delimiter = ',') #ogni riga è una lista. quindi posso prendere ogni elemento con gli indici[]
+#table = csv.DictReader (dataset, delimiter =',') #aggiunge anche l'header ad ogni riga -- per chiamare gli elementi nelle righe non posso usare gli indici ma il nome che sta nell'header
 
+#printo ogni riga nella tabella
 for row in table:
-  print row
+  print (row) 
+  #print (row['coordy'])
+  
+#creo una lista per l'header
+header = ['id', 'coordx', 'coordy', 'h')
+#creo una lista contenente tutte le altre righe del file. devo prima creare una lista vuota
+l = []
+for row in table:  #per ogni riga della mia tabella
+  l.append(row) #aggiungi la riga alla mia lista vuota
+print(l) #ho la mia lista di liste
+          
+#creo un nuovo file csv  -- cambio il nome ma stessa directory!        
+out_table = open("C:/Users/UTENTE/Desktop/UNIBO/GEOGRAFIA/II ANNO/LAB/python/montecristo_point_header.csv")
+new_tab = csv.writer (out_table, delimiter = ',') #creo una nuova tabella 
+#aggiungo l'header
+new_table.writerow(header) #il comando è writerow, della libreria csv
+#aggiungo la lista di liste
+new_table.writerows(l) #al plurale: writerows
+          
+#devo sempre chiudere tutto dopo
+dataset.close()
+out_table.close()
+          
 
